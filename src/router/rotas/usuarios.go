45ab850e250e2cb4267018bbfa002770df249751
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-05-26 22:39:31 -0300

    Dando sequencia no curso

--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -16,7 +16,7 @@ var rotasUrusarios = []Rota{
 		URI:                "/usuarios",
 		Metodo:             http.MethodGet,
 		Funcao:             controllers.BuscarUsuarios,
-		RequerAutenticacao: false,
+		RequerAutenticacao: true,
 	},
 	{
 		URI:                "/usuarios/{usuarioId}",
